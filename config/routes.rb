7ed0dae64e1e7cee2d0da3559ee9616bb1a71a97
Gennaro Anzalone <g.anzalone12@gmail.com>
2017-12-01 15:05:00 -0800

    organize file

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-  resources :reviews
-  devise_for :users
   root :to => 'pages#home'
 
+  resources :reviews
   resources :others
   resources :edibles
   resources :pre_rolls
   resources :flowers
   resources :indicas
   resources :sativas
+
+  devise_for :users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
