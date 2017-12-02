4f902e304c26f5b2f569a0641f6ea4595b9932d2
Gennaro Anzalone <g.anzalone12@gmail.com>
2017-12-01 16:00:09 -0800

    add admin auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :is_admin?
+
+  def is_admin?
+    current_user && current_user.admin
+  end
+  
 end
