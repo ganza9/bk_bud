ffff768e40b922ac5e037319a342d52a48ae51f2
Guest User <Guest@epicodus-118.local>
2017-12-06 09:26:05 -0800

    seed a flower

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Flower.create(name: "Blue Dream", price: 14, thc: 18, description: "Blueberry x Haze. A West Coast classic, this potent sativa provides happy, uplifting, and euphoric effects without heavy sedation.")
