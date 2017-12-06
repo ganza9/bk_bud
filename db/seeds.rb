92cf4d89d1c28a0da018e8cb5a3f8544346df1d1
Gennaro Anzalone <g.anzalone12@gmail.com>
2017-12-06 09:57:35 -0800

    seed one product for each category

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,6 @@
 
 
 Flower.create(name: "Blue Dream", price: 14, thc: 18, description: "Blueberry x Haze. A West Coast classic, this potent sativa provides happy, uplifting, and euphoric effects without heavy sedation.")
+Edible.create(name: "Caramels", price: 10, thc: 10, description: "Creamy, smooth raspberry and lemon caramels. These caramels will elevate you to cloud nine. 2-pack, 4-pack, and 10-pack.")
+Other.create(name: "Fairwinds Flow CBD Gel", price: 47, thc: 150, description: "A unique formula that is designed to penetrate tissues much faster than conventional oil or water based topicals. A synergistic blend of herbal and cannabis extracts that delivers exceptional results with a warming sensation you can feel.")
+PreRoll.create(name: "Gorilla Glue", price: 47, thc: 24, description: "Sour Dub x Chems Sister. This indica-dominant hybrid has a piney, fruity aroma and provides heavy-handed effects. Great for deep relaxation or sedation.")
