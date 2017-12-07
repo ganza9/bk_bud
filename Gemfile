0018d91904e7cc3fee6e023b079b172256cee481
Gennaro Anzalone <g.anzalone12@gmail.com>
2017-12-06 16:45:59 -0800

    add pry and if elsif in reviews

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ group :development, :test do
   gem 'capybara', '~> 2.13'
   gem 'selenium-webdriver'
   gem 'rails-erd', require: false
+  gem 'pry'
 end
 
 group :development do
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,6 +2,9 @@ class ReviewsController < ApplicationController
   skip_before_action :authenticate_user!, :except => [:show]
   #
   before_action :only => [:new, :edit] do
+      redirect_to new_user_session_path unless current_user
+    end
+  before_action :only => [:edit] do
       redirect_to new_user_session_path unless current_user && current_user.admin
     end
 
@@ -14,11 +17,13 @@ class ReviewsController < ApplicationController
   # GET /reviews/1
   # GET /reviews/1.json
   def show
+    @review = Review.find(params[:id])
   end
 
   # GET /reviews/new
   def new
     @review = Review.new
+    binding.pry
   end
 
   # GET /reviews/1/edit
@@ -28,9 +33,22 @@ class ReviewsController < ApplicationController
   # POST /reviews
   # POST /reviews.json
   def create
-    @review = Review.new(review_params)
+    if params[:flower_id]
+      @flower = Flower.find(params[:flower_id])
+      @review = @flower.review.new(review_params)
+    elsif params[:edible_id]
+      @edible = Edible.find(params[:edible_id])
+      @review = @edible.review.new
+    elsif params[:other_id]
+      @other = Other.find(params[:other_id])
+      @review = @other.review.new
+    elsif params[:pre_roll_id]
+      @pre_roll = PreRoll.find(params[:pre_roll_id])
+      @review = @pre_roll.review.new
+    end
 
     respond_to do |format|
+      binding.pry
       if @review.save
         format.html { redirect_to @review, notice: 'Review was successfully created.' }
         format.json { render :show, status: :created, location: @review }
@@ -73,6 +91,6 @@ class ReviewsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def review_params
-      params.require(:review).permit(:reviewer, :review_content)
+      params.require(:review).permit(:reviewer, :review_content, :flower_id)
     end
 end
